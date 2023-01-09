1e7e5b18147ef9d8a45d7defdd6ce8cfab551d8d
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-01-09 12:31:02 +0530

    changes to fix styling issues

--- src/components/MessageComposer.js
+++ src/components/MessageComposer.js
@@ -3,15 +3,19 @@ import Input from "./Input";
 
 const MessageComposer = ({ msgTxt, setMsgTxt, sendNewMessage }) => {
   return (
-    <div className="flex msg-composer">
-      <Input
-        setNewValue={setMsgTxt}
-        placeholder="Write a message"
-        value={msgTxt}
-      />
-      <button className="btn" onClick={sendNewMessage}>
-        Send
-      </button>
+    <div className='flex msg-composer'>
+      <div className='flex-1'>
+        <Input
+          setNewValue={setMsgTxt}
+          placeholder='Write a message'
+          value={msgTxt}
+        />
+      </div>
+      <div>
+        <button className='btn' onClick={sendNewMessage}>
+          Send
+        </button>
+      </div>
     </div>
   );
 };
